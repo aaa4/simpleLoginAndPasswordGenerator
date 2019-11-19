d9b219f9672c32400c6d03ab02f32ed9d4001f86
aaa4 <a.j.lapin1@gmail.com>
2019-11-20 00:51:01 +0300

    add bcrypt encoding

--- src/main/java/passwGen/WriteLoginAndPassFile.java
+++ src/main/java/passwGen/WriteLoginAndPassFile.java
@@ -1,11 +1,23 @@
 package passwGen;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import java.io.*;
+
+
 
 public class WriteLoginAndPassFile {
 
-    private static final int NUMBER_OF_LOGINS = 100;
+    private static   int numberOfLogins = 100;
+    private static int harness = 4;
 
+    public static void setHarness(int harness) {
+        WriteLoginAndPassFile.harness = harness;
+    }
+
+    public static void setNumberOfLogins(int numberOfLogins) {
+        WriteLoginAndPassFile.numberOfLogins = numberOfLogins;
+    }
 
     public static String getLogin(){
         SimpleLoginGenerator loginGenerator = new SimpleLoginGenerator();
@@ -18,13 +30,28 @@ public class WriteLoginAndPassFile {
     }
 
     public static String getLoginAndPasswordSequence(){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(harness);
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < NUMBER_OF_LOGINS; i++) {
+        sb.append("#");
+        sb.append(" ");
+        sb.append("login");
+        sb.append("      ");
+        sb.append("password");
+        sb.append("      ");
+        sb.append("encoded("+harness+") password");
+        sb.append("\n");
+        for (int i = 0; i < numberOfLogins; i++) {
+
+
+
             sb.append(i);
             sb.append(" ");
             sb.append(getLogin());
             sb.append(" ");
-            sb.append(getPassword());
+            String pwd = getPassword();
+            sb.append(pwd);
+            sb.append(" ");
+            sb.append(encoder.encode(pwd));
             sb.append("\n");
         }
         return sb.toString();
@@ -46,28 +73,69 @@ public class WriteLoginAndPassFile {
     }
 
 
-    public static String getFilenmame() throws IOException {
-        String filename;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        filename = reader.readLine();
-        reader.close();
-        return filename;
+
+
+
+
+    public static String readWordFromConsole() throws IOException {
+        String word = "";
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+            word = reader.readLine();
+        }
+        return word;
     }
 
     public static void doIt(){
-        printUserGreeting();
+
         try {
-            String filename = getFilenmame();
+            System.out.println("Enter number of logins");
+            int numberOfLogins = Integer.parseInt(readWordFromConsole());
+            setNumberOfLogins(numberOfLogins);
+            printUserGreeting();
+            String filename = readWordFromConsole();
+            System.out.println("Enter encryption harness from 1 to 10");
+            int harness = Integer.parseInt(readWordFromConsole());
+            setHarness(harness);
+            System.out.println("Encryption harness is "+harness);
             writeLoginAndPasswordToFile(filename);
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void doSomethingElse(){
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+            System.out.println("Enter number of logins");
+            int numberOfLogis = Integer.parseInt(reader.readLine());
+            setNumberOfLogins(numberOfLogis);
+            System.out.println("Number of logins = ");
+
+
+            System.out.println("Enter encryption harness");
+            int harness = Integer.parseInt(reader.readLine());
+            setHarness(harness);
+            System.out.println("Harness is "+harness);
+
+            printUserGreeting();
+            String filename = reader.readLine();
+            System.out.println("Filename = "+filename);
+
+            writeLoginAndPasswordToFile(filename);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
 
+
+
+
     public static void main(String[] args)  {
-        doIt();
+        //doIt();
+        doSomethingElse();
 
     }
 }
